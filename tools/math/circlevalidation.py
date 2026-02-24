ba01433f3ac87ec209acb0634b594bd2a88e6255
Vejtah <vit@palaga.sk>
2026-02-24 11:28:23 +0100

    Create circlevalidation.py

---
+++ tools/math/circlevalidation.py
@@ -0,0 +1,3 @@
+
+
+while True: print(True) if (int(input("x coordinate for validation: ").strip()) - (-75))**2 + (int(input("y coordinate for validation: ").strip()) - (-75))**2 == 100**2 else print(False)
